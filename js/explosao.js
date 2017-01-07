fbb6c8c05054422393f0c69c7626965f9cf00674
Elton Santos <eltonaxl@hotmail.com>
2017-01-07 04:37:08 -0300

    First commit with game finished

---
+++ js/explosao.js
@@ -0,0 +1,30 @@
+var SOM_EXPLOSAO = new Audio();
+SOM_EXPLOSAO.src = "snd/explosao.mp3";
+SOM_EXPLOSAO.volume = 0.3;
+SOM_EXPLOSAO.load();
+
+function Explosao (context, imagem, x, y) {
+  this.context = context;
+  this.imagem = imagem;
+  this.spritesheet = new Spritesheet(context, imagem, 1, 5);
+  this.spritesheet.intervalo = 75;
+  this.x = x;
+  this.y = y;
+  var explosao = this;
+  this.spritesheet.fimDoCiclo = function () {
+    explosao.animacao.excluirSprite(explosao);
+    if (explosao.fimDaExplosao) explosao.fimDaExplosao();
+  }
+  SOM_EXPLOSAO.currentTime = 0.0;
+  SOM_EXPLOSAO.play();
+}
+
+Explosao.prototype = {
+  atualizar: function () {
+    
+  },
+  desenhar: function () {
+    this.spritesheet.desenhar(this.x, this.y);
+    this.spritesheet.proximoQuadro();
+  }
+}
